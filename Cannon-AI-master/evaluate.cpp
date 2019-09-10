9f30b3f7b9b608e146a5ffc1a245ba388634c218
Shreya Sharma <34606101+ShreyaSharma99@users.noreply.github.com>
2019-09-10 21:10:45 +0600

    Compiled

--- Cannon-AI-master/evaluate.cpp
+++ Cannon-AI-master/evaluate.cpp
@@ -1,158 +1,180 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int s1,s2,t1.t2;
-int** game_state;
+int s1,s2,t1,t2;
+vector< vector<int> > game_state;
 int m,n;
-//Assuming we have a struct of game_state with varibales for the count of soldiers and towers as well
-int Evaluation_func(int** game_state, int soldier1, int soldier2, int tower1, int tower2){
-	int m = game_state.size();
-	int n = game_state[0].size();
 
-	void Targets(int i, int j, bool you, string str, int* target_soldier, int* target_tower){
-		int soldiers = 1;
+vector<int> Targets(int i, int j, bool you, string str){
+		int soldier = 1;
 		int tower = 2;
+		int target_soldier=0;
+		int target_tower =0;
+		vector<int> ans(2);
 		if(!you){
-			soldiers = -1;
+			soldier = -1;
 			tower = -2;
 		}
 
-		if(str.equals("vert")){
+		if(str.compare("vert")==0){
 			if(i-2>=0){
-				if(self.state[i-2][j] == soldier)
+				if(game_state[i-2][j] == soldier)
 					target_soldier+=1;
-				else if(self.state[i-2][j] == tower)
+				else if(game_state[i-2][j] == tower)
 					target_tower+=1;
 			}
 			if(i-1>=0){
-				if(self.state[i-1][j] == soldier)
+				if(game_state[i-1][j] == soldier)
 					target_soldier+=1;
-				else if(self.state[i-1][j] == tower)
+				else if(game_state[i-1][j] == tower)
 					target_tower+=1;
 			}
 			if(i+2>=0){
-				if(self.state[i+2][j] == soldier)
+				if(game_state[i+2][j] == soldier)
 					target_soldier+=1;
-				else if(self.state[i+2][j] == tower)
+				else if(game_state[i+2][j] == tower)
 					target_tower+=1;
 			}			
 			if(i+1>=0){
-				if(self.state[i+1][j] == soldier)
+				if(game_state[i+1][j] == soldier)
 					target_soldier+=1;
-				else if(self.state[i+1][j] == tower)
+				else if(game_state[i+1][j] == tower)
 					target_tower+=1;
 			}
 		}
 
-		if(str.equals("horz")){
+		if(str.compare("horz")==0){
 			if(j-2>=0){
-				if(self.state[i][j-2] == soldier)
+				if(game_state[i][j-2] == soldier)
 					target_soldier+=1;
-				else if(self.state[i][j-2] == tower)
+				else if(game_state[i][j-2] == tower)
 					target_tower+=1;
 			}
 			if(j-1>=0){
-				if(self.state[i][j-1] == soldier)
+				if(game_state[i][j-1] == soldier)
 					target_soldier+=1;
-				else if(self.state[i][j-1] == tower)
+				else if(game_state[i][j-1] == tower)
 					target_tower+=1;
 			}
 			if(j+2>=0){
-				if(self.state[i][j+2] == soldier)
+				if(game_state[i][j+2] == soldier)
 					target_soldier+=1;
-				else if(self.state[i][j+2] == tower)
+				else if(game_state[i][j+2] == tower)
 					target_tower+=1;
 			}			
 			if(j+1>=0){
-				if(self.state[i][j+1] == soldier)
+				if(game_state[i][j+1] == soldier)
 					target_soldier+=1;
-				else if(self.state[i][j+!] == tower)
+				else if(game_state[i][j+1] == tower)
 					target_tower+=1;
 			}
 		}
 
-		if(str.equals("rdiag")){
+		if(str.compare("rdiag")==0){
 			if(j-2>=0 and i+2<=m-1){
-				if(self.state[i+2][j-2] == soldier)
+				if(game_state[i+2][j-2] == soldier)
 					target_soldier+=1;
-				else if(self.state[i+2][j-2] == tower)
+				else if(game_state[i+2][j-2] == tower)
 					target_tower+=1;
 			}
 			if(j-1>=0 and i+1<=m-1){
-				if(self.state[i+1][j-1] == soldier)
+				if(game_state[i+1][j-1] == soldier)
 					target_soldier+=1;
-				else if(self.state[i+1][j-1] == tower)
+				else if(game_state[i+1][j-1] == tower)
 					target_tower+=1;
 			}
 			if(j+2<=n-1 and i-2>=0){
-				if(self.state[-2i][j+2] == soldier)
+				if(game_state[i-2][j+2] == soldier)
 					target_soldier+=1;
-				else if(self.state[i-2][j+2] == tower)
+				else if(game_state[i-2][j+2] == tower)
 					target_tower+=1;
 			}			
 			if(j+1<=n-1 and i-1>=0){
-				if(self.state[i-1][j+1] == soldier)
+				if(game_state[i-1][j+1] == soldier)
 					target_soldier+=1;
-				else if(self.state[i-1][j+!] == tower)
+				else if(game_state[i-1][j+1] == tower)
 					target_tower+=1;
 			}
 		}
 
-		if(str.equals("ldiag")){
+		if(str.compare("ldiag")==0){
 			if(j-2>=0 and i-2>=0){
-				if(self.state[i-2][j-2] == soldier)
+				if(game_state[i-2][j-2] == soldier)
 					target_soldier+=1;
-				else if(self.state[i-2][j-2] == tower)
+				else if(game_state[i-2][j-2] == tower)
 					target_tower+=1;
 			}
 			if(j-1>=0 and i-1>=0){
-				if(self.state[i-1][j-1] == soldier)
+				if(game_state[i-1][j-1] == soldier)
 					target_soldier+=1;
-				else if(self.state[i-1][j-1] == tower)
+				else if(game_state[i-1][j-1] == tower)
 					target_tower+=1;
 			}
 			if(j+2<=n-1 and i+2<=m-1){
-				if(self.state[i+2][j+2] == soldier)
+				if(game_state[i+2][j+2] == soldier)
 					target_soldier+=1;
-				else if(self.state[i+2][j+2] == tower)
+				else if(game_state[i+2][j+2] == tower)
 					target_tower+=1;
 			}			
 			if(j+1<=n-1 and i+1<=m-1){
-				if(self.state[i+1][j+1] == soldier)
+				if(game_state[i+1][j+1] == soldier)
 					target_soldier+=1;
-				else if(self.state[i+1][j+!] == tower)
+				else if(game_state[i+1][j+1] == tower)
 					target_tower+=1;
 			}
 		}
+
+		ans[0] = target_soldier;
+		ans[1] = target_tower;
+		return ans;
 	}
 
-	void cannon_effect(int*  cannon_count, int* target_soldier, int* target_tower, int player){
-		cannon_count = 0
-		targer_soldier = 0
-		target_tower = 0
+	vector<int> cannon_effect(int player){
+		int cannon_count = 0;
+		int targer_soldier = 0;
+		int target_tower = 0;
+		vector<int> counts(2);
+		vector<int> ans(3);
 		for(int i=1; i<m-1; i++){
 			for(int j=1; j<n-1; j++){
 				if(game_state[i][j]==player){
 					if(game_state[i-1][j] == player  && game_state[i+1][j] == player){
-						cannon_count+=1
-						targets(i, j, player>0, "horz", target_soldier, target_tower);
+						(cannon_count)+=1;
+						counts = Targets(i, j, player>0, "horz");
 					}
 					else if(game_state[i][j-1] == player  and game_state[i][j+1] == player){
-						cannon_count+=1
-						targets(i, j, player>0, "vert", target_soldier, target_tower);
+						(cannon_count)+=1;
+						counts = Targets(i, j, player>0, "vert");
 					}
 					else if(game_state[i-1][j+1] == player  and game_state[i+1][j-1] == player){
-						cannon_count+=1
-						targets(i, j, player>0, "rdiag", target_soldier, target_tower);
+						(cannon_count)+=1;
+						counts = Targets(i, j, player>0, "rdiag");
 					}
 					else if(game_state[i-1][j-1] == player  and game_state[i+1][j+1] == player){
-						cannon_count+=1
-						targets(i, j, player>0, "ldiag", target_soldier, target_tower);
+						(cannon_count)+=1;
+						counts = Targets(i, j, player>0, "ldiag");
 					}
 				}
 			}
 		}
+		ans[0] = cannon_count;
+		ans[1] = counts[0];
+		ans[2] = counts[1];
+
+		return ans;
 	}
+
+//Assuming we have a struct of game_state with varibales for the count of soldiers and towers as well
+int Evaluation_func(vector< vector<int> > state, int soldier1, int soldier2, int tower1, int tower2){
+	m = game_state.size();
+	n = game_state[0].size();
+
+	s1 = soldier1;
+	s2 = soldier2;
+	t1 = tower1;
+	t2 = tower2;
+
+	game_state = state;
 
 	int cannon_count_us=0;
 	int target_soldier_us = 0;
@@ -162,10 +184,17 @@ int Evaluation_func(int** game_state, int soldier1, int soldier2, int tower1, in
 	int target_soldier_opp = 0;
 	int target_tower_opp = 0;
 
-	cannon_effect(cannon_count_us, target_soldier_opp, target_tower_opp, 1);
-	cannon_effect(cannon_count_opp, target_soldier_us, target_tower_us, -1);
+	vector<int> ans(3);
+	ans = cannon_effect(1);
+	cannon_count_us = ans[0];
+	target_soldier_opp = ans[1];
+	target_tower_opp = ans[2];
+	
+	ans = cannon_effect( -1);
+	cannon_count_opp = ans[0];
+	target_soldier_us = ans[1];
+	target_tower_us = ans[2];
 
-	int value = (s1*10) + (t1*100) - (s2*10) - (t2*100) + (target_soldier_opp*5) + (target_tower_opp*50) - (targer_soldier_us*5) - (target_tower_us*50);
+	int value = (s1*10) + (t1*100) - (s2*10) - (t2*100) + (target_soldier_opp*5) + (target_tower_opp*50) - (target_soldier_us*5) - (target_tower_us*50);
 	return value;
-
 }
